16cd207c91b7e04726296972d5a6b575d8b0690b
OfficieelJoran <91888385+OfficieelJoran@users.noreply.github.com>
2022-06-23 13:30:30 +0200

    Add files via upload

---
+++ BKEvragen/index.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FAQ PAGE</title>
+    <link href="https://fonts.googleapis.com/css2?family=Work+Sans&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <main>
+        <h1 class="faq-heading">Vragen en Antwoorden</h1>
+        <section class="faq-container">
+            <div class="faq-one">
+                <!-- faq question -->
+                <h1 class="faq-page">Welke taal word voornamelijk gebruikt voor het koppelen van databases?<br> A HTML <br> B CSS <br> C PHP</h1>
+                <!-- faq answer -->
+                <div class="faq-body">
+                    <p>Het antwoord is A PHP</p>
+                </div>
+            </div>
+            <hr class="hr-line">
+            <div class="faq-two">
+                <!-- faq question -->
+                <h1 class="faq-page">hoeveel verdiend een gemiddelde MBO software Developer per jaar?<br> A €62.000,-<br> B €40.500,- <br> C €84.365,- </h1>
+                <!-- faq answer -->
+                <div class="faq-body">
+                    <p>Het antwoord is B €40.500,-</p>
+                </div>
+            </div>
+            <hr class="hr-line">
+            <div class="faq-three">
+                <!-- faq question -->
+<h1 class="faq-page">Waar kan een Software Developer niet zonder?<br> A Zijn Zonnebril <br> B een stoel <br> C Zijn oplader	</h1>
+                <!-- faq answer -->
+                <div class="faq-body">
+                    <p>Het antwoord is C Zijn oplader</p>
+                </div>
+            </div>
+            <hr class="hr-line">
+            <div class="faq-three">
+                <!-- faq question -->
+<h1 class="faq-page">Waar hebben Software Developers te kort aan?<br> A Een vriendin <br> B Redbull <br> C Schermtijd</h1>
+                <!-- faq answer -->
+                <div class="faq-body">
+                    <p>Het antwoord is A Een vriendin</p>
+                </div>
+            </div>
+            <hr class="hr-line">
+            <div class="faq-three">
+                <!-- faq question -->
+<h1 class="faq-page">Wat is een handige agenda applicatie voor Software Developers?<br> A Wish <br> B Trello <br> C calculator </h1>
+                <!-- faq answer -->
+                <div class="faq-body">
+                    <p>Het antwoord is B Trello</p>
+                </div>
+            </div>
+            <hr class="hr-line">
+            <div class="faq-three">
+                <!-- faq question -->
+<h1 class="faq-page">Hoelang duurt 1 challenge?<br> A 6 maanden <br> B een halfuurtje <br> C 3 weken </h1>
+                <!-- faq answer -->
+                <div class="faq-body">
+                    <p>Het antwoord is C 3 Weken</p>
+                </div>
+            </div>
+            <hr class="hr-line">
+            <div class="faq-three">
+                <!-- faq question -->
+<h1 class="faq-page">Hoeveel euro heeft Vista Sittard uitgegeven aan ultrawide schermen?<br> A Te weinig <br> B Te veel <br> C Genoeg</h1>
+                <!-- faq answer -->
+                <div class="faq-body">
+                    <p>Het antwoord is B Te veel</p>
+                </div>
+            </div>
+            <hr class="hr-line">
+            <div class="faq-three">
+                <!-- faq question -->
+<h1 class="faq-page">Welke docent praat het meest?	<br> A Meneer Backus <br> B Mevrouw Khazali<br> C Meneer Siebert</h1>
+                <!-- faq answer -->
+                <div class="faq-body">
+                    <p>Het antwoord is A Meneer Backus</p>
+                </div>
+            </div>
+            <hr class="hr-line">
+            <div class="faq-three">
+                <!-- faq question -->
+<h1 class="faq-page">Welke docent heeft de Bender app gemaakt?<br> A Mevrouw Kerckoffs <br> B Meneer Siebert <br> C Meneer Steens </h1>
+                <!-- faq answer -->
+                <div class="faq-body">
+                    <p>Het antwoord is B Meneer Siebert</p>
+                </div>
+            </div>
+            <hr class="hr-line">
+            <div class="faq-three">
+                <!-- faq question -->
+<h1 class="faq-page">Wat is onzichtbaar en niet te vinden op Vista?<br> A Een lamp <br> B Motivatie <br> C Fatsoenlijke Wifi </h1>
+                <!-- faq answer -->
+                <div class="faq-body">
+                    <p>Het antwoord is C Fatsoenlijke Wifi</p>
+                </div>
+            </div>
+            <hr class="hr-line">
+        </section>
+    </main>
+    <script src="main.js"></script>
+</body>
+</html>
